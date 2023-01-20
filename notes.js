bee241fe59d9369618a39198d7034aa58faf010f
ABIPRAVI <darkparadise877@gmail.com>
2023-01-20 06:05:35 +0530

    Update firebase

--- notes.js
+++ notes.js
@@ -1,14 +1,29 @@
+function generateItems(data1) {
+	data1.forEach((item) => {
+		document.getElementById(
+			'notesdata',
+		).innerHTML += `<a href="${item.file}" target="_blank" style="text-decoration:none">
+		<div class="card" style="width: 18rem; height:150px">
+        <div class="card-body">
+          <h5 class="card-title text-dark">${item.title}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">${item.user}</h6>
+          <p class="card-text">${item.description}</p>
+        </div>
+      </div>
+		</a>`;
+	});
+}
+
 function getItems() {
 	db.collection('notes').onSnapshot((snapshot) => {
 		let items = [];
-		console.log(snapshot);
 		snapshot.docs.forEach((doc) => {
 			items.push({
 				id: doc.id,
 				...doc.data(),
 			});
 		});
-		console.log(items);
+		generateItems(items);
 	});
 }
 
